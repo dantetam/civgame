1bb75f76d4d1bde49e3789ca7f8934a0062c8ddf
igetshotinmydreams <dante.tam1@gmail.com>
2014-09-01 21:42:20 -0700

    Updated menu

--- src/render/Game.java
+++ src/render/Game.java
@@ -6,6 +6,7 @@ import processing.core.PApplet;
 public class Game extends PApplet {
 
 	public String gameMode = "MainMenu";
+	public String challengeType = "";
 	public ArrayList<Menu> menus;
 	public Menu activeMenu;
 
@@ -14,14 +15,22 @@ public class Game extends PApplet {
 		size(1600,900);
 		menus = new ArrayList<Menu>();
 
-		Menu menu1 = new Menu("MainMenu");
-		menus.add(menu1);
-		menu1.addButton("newgame", "New Game", 100, 100, 210, 70);
-		menu1.addButton("exitgame", "Exit Game", 100, 700, 210, 70);
+		Menu menu0 = new Menu("MainMenu");
+		menus.add(menu0);
+		menu0.addButton("newgame", "New Game", 100, 100, 210, 70);
+		menu0.addButton("exitgame", "Exit Game", 100, 700, 210, 70);
 		//menu1.on();
 
+		Menu menu1 = new Menu("ChallengeTypeMenu");
+		menus.add(menu1);
+		menu1.addButton("conquestgame", "Conquest", 100, 100, 210, 70);
+		menu1.addButton("survivalgame", "Survival", 100, 200, 210, 70);
+		
 		Menu menu2 = new Menu("TerrainMenu");
 		menus.add(menu2);
+		menu2.addButton("", "Archipelago", 100, 100, 210, 70);
+		menu2.addButton("", "Island Chain", 100, 200, 210, 70);
+		menu2.addButton("", "Rolling Hills", 100, 300, 210, 70);
 		//menu2.addButton("newgame", "New Game", 100, 100, 210, 70);
 	}
 
@@ -33,9 +42,13 @@ public class Game extends PApplet {
 		{
 			activeMenu = menus.get(0);
 		} 
-		else if (gameMode.equals("TerrainMenu"))
+		else if (gameMode.equals("ChallengeTypeMenu"))
 		{
 			activeMenu = menus.get(1);
+		}
+		else if (gameMode.equals("TerrainMenu"))
+		{
+			activeMenu = menus.get(2);
 		}
 		for (int i = 0; i < activeMenu.buttons.size(); i++)
 		{
@@ -55,9 +68,29 @@ public class Game extends PApplet {
 			if (menus.get(i).equals(activeMenu))
 			{
 				String command = menus.get(i).click(mouseX, mouseY);
-				if (command != null)
+				if (command != null && !command.equals(""))
 				{
-					println(command);
+					if (command.equals("newgame"))
+					{
+						gameMode = "ChallengeTypeMenu";
+					}
+					else if (command.equals("exitgame"))
+					{
+						System.exit(0);
+					}
+					else if (command.equals("conquestgame"))
+					{
+						challengeType = "Conquest";
+						gameMode = "TerrainMenu";
+						redraw();
+					}
+					else if (command.equals("survivalgame"))
+					{
+						challengeType = "Survival";
+						gameMode = "TerrainMenu";
+						redraw();
+					}
+					
 				}
 			}
 		}
