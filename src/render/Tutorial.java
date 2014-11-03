1ce5a4ead4eab968a637ce36d2548569ec2b62cb
dantetam <dante.tam1@gmail.com>
2014-11-03 12:11:10 -0800

    Restriction of keys during tutorial

--- src/render/Tutorial.java
+++ src/render/Tutorial.java
@@ -11,25 +11,34 @@ public class Tutorial extends CivGame {
 	public Game game;
 	public int step = -1;
 	public ArrayList<ArrayList<Character>> path;
-	
+	public ArrayList<String> cond;
+	public boolean[] keysAllowed = new boolean[200];
+
 	public Tutorial(Game game, float width, float height)
 	{
 		super(game, 2, 1, "survival", "terrain11", 8700L);
+		keysAllowed[97] = true;
 		path = new ArrayList<ArrayList<Character>>();
+		cond = new ArrayList<String>();
+
 		path.add(list(32));
+		cond.add("");
+		
+		path.add(list(200));
+		cond.add("");
 	}
-	
+
 	public void setup()
 	{
 		super.setup();
 		step();
 	}
-	
+
 	public void draw()
 	{
 		super.draw();
 	}
-	
+
 	public ArrayList<Character> list(char... keys)
 	{
 		ArrayList<Character> temp = new ArrayList<Character>();
@@ -37,7 +46,7 @@ public class Tutorial extends CivGame {
 			temp.add(keys[i]);
 		return temp;
 	}
-	
+
 	public ArrayList<Character> list(int... keys)
 	{
 		ArrayList<Character> temp = new ArrayList<Character>();
@@ -46,6 +55,25 @@ public class Tutorial extends CivGame {
 		return temp;
 	}
 	
+	public ArrayList<Character> empty()
+	{
+		ArrayList<Character> temp = new ArrayList<Character>();
+		return temp;
+	}
+	
+	public void enable(char... keys)
+	{
+		for (int i = 0; i < keys.length; i++)
+			keysAllowed[(int)keys[i]] = true;
+	}
+	
+	public void disable(char... keys)
+	{
+		for (int i = 0; i < keys.length; i++)
+			keysAllowed[(int)keys[i]] = false;
+	}
+
+
 	public void step()
 	{
 		step++;
@@ -54,33 +82,55 @@ public class Tutorial extends CivGame {
 		{
 		case 0:
 			menuSystem.message("Entered tutorial level");
-			menuSystem.message("Press SPACE to continue");
+			menuSystem.message("Press SPACE to cycle through your units");
 			break;
 		case 1:
 			menuSystem.message("End test");
-			println("Step1");
+			enable('w','a','s','d');
 			break;
 		default:
 			break;
 		}
 	}
-	
+
 	public void keyPressed()
 	{
-		if (step >= path.size()) return; //Tutorial is over
-		if (path.get(step).get(0) == key)
+		if (step < path.size())
 		{
-			path.get(step).remove(0);
+			if (path.get(step).size() != 0)
+				if (path.get(step).get(0) == key)
+				{
+					super.keyPressed();
+					path.get(step).remove(0);
+				}
+		}
+		if (keysAllowed[(int)key])
+		{
+			super.keyPressed();
+		}
+		//else
+			//return; //Wrong key pressed. Restrict access to other keys
+		if (done(cond.get(step)))
 			if (path.get(step).size() == 0)
 				step();
-		}
-		else
-			return; //Wrong key pressed. Restrict access to other keys
 	}
-	
+
 	public void keyReleased()
 	{
 		super.keyReleased();
 	}
 	
+	public boolean done(String c)
+	{
+		if (c == null || c.equals(""))
+		{
+			return true;
+		}
+		else
+		{
+			//Invalid
+			return true;
+		}
+	}
+
 }
