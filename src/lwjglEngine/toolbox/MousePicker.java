26ca5df9abae1562caf790dd7cbbd066c39e6218
dantetam <dante.tam1@gmail.com>
2015-10-15 16:56:31 -0700

    Implementing mouse picking

---
+++ src/lwjglEngine/toolbox/MousePicker.java
@@ -0,0 +1,47 @@
+package lwjglEngine.toolbox;
+
+import org.lwjgl.input.Mouse;
+import org.lwjgl.opengl.Display;
+import org.lwjgl.util.vector.Matrix4f;
+import org.lwjgl.util.vector.Vector2f;
+import org.lwjgl.util.vector.Vector3f;
+import org.lwjgl.util.vector.Vector4f;
+
+import lwjglEngine.entities.Camera;
+
+//Also courtesy of ThinMatrix.
+
+public class MousePicker {
+
+	public Vector3f currentRay;
+	
+	private Matrix4f projMatrix, viewMatrix;
+	private Camera camera;
+	
+	public MousePicker(Matrix4f p, Camera c) {
+		camera = c;
+		projMatrix = p;
+		viewMatrix = Maths.createViewMatrix(camera);
+	}
+	
+	public void update()
+	{
+		viewMatrix = Maths.createViewMatrix(camera);
+		currentRay = calculateMouseRay();
+	}
+	
+	private Vector3f calculateMouseRay()
+	{
+		float mouseX = Mouse.getX(), mouseY = Mouse.getY();
+		float normalX = 2f*mouseX/Display.getWidth() - 1f;
+		float normalY = 2f*mouseY/Display.getHeight() - 1f;
+		Vector2f normalized = new Vector2f(normalX, normalY);
+		Vector4f clip = new Vector4f(normalized.x, normalized.y, -1f, 1f);
+		Vector4f eye = Matrix4f.transform(Matrix4f.invert(projMatrix, null), clip, null);
+		eye.z = -1f; eye.w = 0f;
+		Vector4f temp = Matrix4f.transform(Matrix4f.invert(viewMatrix, null), eye, null);
+		Vector3f rayWorld = new Vector3f(temp.x, temp.y, temp.z);
+		return (Vector3f)rayWorld.normalise();
+	}
+
+}
