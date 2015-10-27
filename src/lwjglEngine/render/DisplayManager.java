873aed59f6ffa2cb38e6cbb7706f58ed34fe82ea
dantetam <dante.tam1@gmail.com>
2015-10-27 01:47:23 -0700

    Reorganizing inputs + key event callbacks

--- src/lwjglEngine/render/DisplayManager.java
+++ src/lwjglEngine/render/DisplayManager.java
@@ -1,25 +1,38 @@
 package lwjglEngine.render;
 
+import static org.lwjgl.glfw.GLFW.GLFW_CONTEXT_VERSION_MAJOR;
+import static org.lwjgl.glfw.GLFW.GLFW_CONTEXT_VERSION_MINOR;
+import static org.lwjgl.glfw.GLFW.GLFW_KEY_SPACE;
+import static org.lwjgl.glfw.GLFW.GLFW_OPENGL_CORE_PROFILE;
+import static org.lwjgl.glfw.GLFW.GLFW_OPENGL_PROFILE;
+import static org.lwjgl.glfw.GLFW.GLFW_RELEASE;
+import static org.lwjgl.glfw.GLFW.GLFW_RESIZABLE;
+import static org.lwjgl.glfw.GLFW.glfwCreateWindow;
+import static org.lwjgl.glfw.GLFW.glfwDestroyWindow;
+import static org.lwjgl.glfw.GLFW.glfwInit;
+import static org.lwjgl.glfw.GLFW.glfwMakeContextCurrent;
+import static org.lwjgl.glfw.GLFW.glfwPollEvents;
+import static org.lwjgl.glfw.GLFW.glfwSetErrorCallback;
+import static org.lwjgl.glfw.GLFW.glfwSetKeyCallback;
+import static org.lwjgl.glfw.GLFW.glfwShowWindow;
+import static org.lwjgl.glfw.GLFW.glfwSwapBuffers;
+import static org.lwjgl.glfw.GLFW.glfwWindowHint;
+import static org.lwjgl.glfw.GLFW.glfwWindowShouldClose;
+import static org.lwjgl.opengl.GL11.GL_FALSE;
+import static org.lwjgl.opengl.GL11.GL_TRUE;
+
 import org.lwjgl.glfw.Callbacks;
-import org.lwjgl.glfw.GLFW;
-import static org.lwjgl.glfw.GLFW.*;
-import org.lwjgl.glfw.GLFWCursorPosCallback;
 import org.lwjgl.glfw.GLFWErrorCallback;
-import org.lwjgl.glfw.GLFWFramebufferSizeCallback;
 import org.lwjgl.glfw.GLFWKeyCallback;
-import org.lwjgl.glfw.GLFWMouseButtonCallback;
-import org.lwjgl.glfw.GLFWvidmode;
-import static org.lwjgl.opengl.GL11.*;
-import static org.lwjgl.opengl.GL15.*;
-import static org.lwjgl.opengl.GL20.*;
-import static org.lwjgl.opengl.GL30.*;
-import org.lwjgl.opengl.*;
+import org.lwjgl.opengl.GL11;
+import org.lwjgl.opengl.GLContext;
 
 public class DisplayManager {
 
 	public static final int width = 1500, height = 900;
 	private static GLFWErrorCallback errorCallback;
 	public static long window;
+	private static GLFWKeyCallback keyCallback;
 	
 	public static void createDisplay()
 	{
@@ -39,6 +52,17 @@ public class DisplayManager {
 		}
 
 		GL11.glViewport(0, 0, width, height);
+		
+		glfwSetKeyCallback(window, (keyCallback = new GLFWKeyCallback() {
+
+		    @Override
+		    public void invoke(long window, int key, int scancode, int action, int mods) {
+		        if(key == GLFW_KEY_SPACE && action == GLFW_RELEASE) {
+
+		        }
+		    }
+
+		}));
 	}
 	
 	public static void updateDisplay()
