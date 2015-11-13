ec34dc724c9e5f019c058c28eb052cfecf855b82
dantetam <dante.tam1@gmail.com>
2015-11-12 23:59:59 -0800

    AI framework

---
+++ src/game_ai/Intelligence.java
@@ -0,0 +1,32 @@
+package game_ai;
+
+import game.Civilization;
+
+//All around wrapper class for advanced AI concepts used in the game
+
+public class Intelligence {
+	
+	public int prosperityScore(Civilization civ)
+	{
+		
+	}
+	
+	public int unitsScore(Civilization civ)
+	{
+		
+	}
+	public int techScore(Civilization civ)
+	{
+		
+	}
+	public int developmentScore(Civilization civ) //Cities, improvements, territory
+	{
+		
+	}
+	
+	public int oneUnitScore(BaseEntity en) //Calculates score of a unit. May be adaptive.
+	{
+		
+	}
+
+}
---
+++ src/game_ai/MaxTree.java
@@ -0,0 +1,9 @@
+package game_ai;
+
+//An expectimax tree data structure used for determining AI action
+
+public class MaxTree {
+
+	public static MaxTree generateTree(Civilization civ) //Generate an expectimax tree based on civilization's choices
+	
+}
